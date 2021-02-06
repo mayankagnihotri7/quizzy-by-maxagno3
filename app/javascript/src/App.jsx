f1c62c54d81ba43142ec2de09e152441bdf815aa
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-02-06 16:27:26 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-      </Switch>
-    </Router>
-  );
+  return <h1>This is App.jsx</h1>;
 };
 
 export default App;
