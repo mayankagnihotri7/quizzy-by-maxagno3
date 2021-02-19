77c8cc53791697d2e9ef29432665d80e54457c65
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-02-19 19:02:26 +0530

    Fix: add validations for options length, add not null constraint on title and destroy for quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,5 @@ class Question < ApplicationRecord
   validates :answer, presence: true
   has_many :options, dependent: :destroy
   accepts_nested_attributes_for :options, allow_destroy: true
+  validates_length_of :options, minimum: 2, maximum: 4
 end
