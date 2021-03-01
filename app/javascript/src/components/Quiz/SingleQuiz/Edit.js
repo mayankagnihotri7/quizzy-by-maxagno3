69a1c27c0337ec36995faa451c7c583015347b2b
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-03-01 22:12:40 +0530

    Fix: Created reusable component

--- app/javascript/src/components/Quiz/SingleQuiz/Edit.js
+++ app/javascript/src/components/Quiz/SingleQuiz/Edit.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Button from "components/Button";
-import Input from "components/Input";
-import Select from "react-select";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import uuid from "react-uuid";
 import questionsApi from "apis/questions";
 import Loader from "../../Common/Loader";
+import QuizForm from "./QuizForm";
 
 const Edit = () => {
   const [correctAnswer, setCorrectAnswer] = useState("");
   const [options_attributes, setOptionsAttributes] = useState([]);
+  const [options, setOptions] = useState([]);
   const [title, setTitle] = useState("");
   const { quiz_id, id } = useParams();
   const [loading, setLoading] = useState(false);
@@ -25,20 +23,39 @@ const Edit = () => {
     const newData = questionDetails.data.options.map(data => {
       return { id: data.id, name: data.name };
     });
+    setOptions(newData);
     setOptionsAttributes(newData);
     setLoading(false);
   };
 
   const handleChange = (index, e) => {
-    const newArr = options_attributes.map(option => {
-      if (option.id === index) {
-        return { ...option, name: e.target.value };
+    let newArr = [...options_attributes];
+    newArr[index].name = e.target.value;
+    setOptions(newArr);
+    setOptionsAttributes(newArr);
+  };
+
+  const handleRemoveOption = (optionId, index) => {
+    const newArr = [...options_attributes];
+    const newOptions = options_attributes.filter((_option, i) => i !== index);
+    const updateOption = newArr.map((option, i) => {
+      if (option.id === optionId || index === i) {
+        return { ...option, _destroy: true };
       } else {
         return option;
       }
     });
+    setOptions(newOptions);
+    setOptionsAttributes(updateOption);
+  };
+
+  const handleAddOption = () => {
+    const newArr = [...options_attributes, { name: "" }];
+    setOptions(newArr);
     setOptionsAttributes(newArr);
   };
+
+  console.log(options_attributes, "from the edit form");
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -50,10 +67,6 @@ const Edit = () => {
     window.location.href = `/quizzes/${quiz_id}/show`;
   };
 
-  const options = options_attributes.map((option, i) => {
-    return { value: option?.name, label: `Option ${i + 1}` };
-  });
-
   useEffect(() => {
     fetchQuestionDetails();
   }, []);
@@ -64,127 +77,17 @@ const Edit = () => {
 
   return (
     <div className="container mx-auto">
-      <form onSubmit={handleSubmit}>
-        <div className="w-1/4">
-          <Input
-            label="Question title"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-          {options_attributes?.map((option, i) => {
-            return (
-              <div key={i}>
-                <div className="flex items-baseline">
-                  <Input
-                    label={`Option ${i + 1}`}
-                    value={option?.name}
-                    onChange={e => handleChange(option.id, e)}
-                  />
-                  {options_attributes.length === 3 &&
-                    options_attributes[2].id === option.id && (
-                    <>
-                      <button
-                        className="text-sm text-center text-bb-red
-                          transition duration-300 ease-in-out
-                          ri-delete-bin-5-line hover:text-bb-red"
-                        onClick={e => {
-                          options_attributes[2]._destroy = true;
-                        }}
-                      >
-                          Delete
-                      </button>
-                    </>
-                  )}
-                  {options_attributes.length === 4 &&
-                    options_attributes[3].id === option.id && (
-                    <>
-                      <button
-                        onClick={e => {
-                          options_attributes[2]._destroy = true;
-                        }}
-                        className="text-sm text-center text-bb-red
-                          transition duration-300 ease-in-out
-                          ri-delete-bin-5-line hover:text-bb-red"
-                      >
-                          Delete
-                      </button>
-                    </>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-          {options_attributes.length === 2 && (
-            <>
-              <div className="flex items-baseline">
-                <label className="mr-2">Option 3:</label>
-                <Input
-                  placeholder="Enter option 3 here (optional)"
-                  required={false}
-                  key={"mayank"}
-                  onChange={e => {
-                    setOptionsAttributes([
-                      ...options_attributes,
-                      {
-                        name: e.target.value,
-                      },
-                    ]);
-                  }}
-                />
-              </div>
-              <div className="flex items-baseline">
-                <label className="mr-2">Option 4:</label>
-                <Input
-                  placeholder="Enter option 4 here (optional)"
-                  required={false}
-                  key="max"
-                  onChange={e => {
-                    setOptionsAttributes([
-                      ...options_attributes,
-                      {
-                        name: e.target.value,
-                      },
-                    ]);
-                  }}
-                />
-              </div>
-            </>
-          )}
-          {options_attributes.length === 3 && (
-            <div className="flex items-baseline" key={uuid()}>
-              <label className="mr-2">Option 4:</label>
-              <Input
-                placeholder="Enter option 4 here(optional)"
-                required={false}
-                onChange={e => {
-                  setOptionsAttributes([
-                    ...options_attributes,
-                    {
-                      name: e.target.value,
-                    },
-                  ]);
-                }}
-              />
-            </div>
-          )}
-          <div className="w-6/12">
-            <Select
-              value={correctAnswer}
-              options={options}
-              onChange={e => setCorrectAnswer(e.value)}
-              placeholder={correctAnswer}
-              className="cursor-pointer"
-            />
-          </div>
-          {(correctAnswer !== "" || correctAnswer !== " ") &&
-            options_attributes[0]?.name !== "" &&
-            options_attributes[1]?.name !== "" && (
-            <div className="w-1/4">
-              <Button type="submit" buttonText="Submit" />
-            </div>
-          )}
-        </div>
-      </form>
+      <QuizForm
+        title={title}
+        setTitle={setTitle}
+        setCorrectAnswer={setCorrectAnswer}
+        options_attributes={options}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleRemoveOption={handleRemoveOption}
+        handleAddOption={handleAddOption}
+        type="Update"
+      />
     </div>
   );
 };
