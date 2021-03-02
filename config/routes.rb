d75aa67414d2765b8d0cece33102ed634574a31c
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2021-03-02 13:50:51 +0530

    Fix: Used resourceful routes for publishing the quiz

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   resource :sessions, only: [:create, :destroy]
   resources :quizzes, only: [:create, :index, :destroy, :update, :show] do
     resources :questions, only: [:create, :index, :update, :show, :destroy]
+    resource :publish, only: [:update], to: "publish"
   end
   resources :public, param: :slug
   resources :attempts, param: :slug
   resource :reports_downloads, only: [:create, :show]
-  patch "/quizzes/:id/publish", to: "publish#publish"
+  # patch "/quizzes/:id/publish", to: "publish#publish"
   get "/reports", to: "reports#index"
   get '*path', to: 'home#index', via: :all
 end
